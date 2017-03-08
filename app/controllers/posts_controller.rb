ca84d8f6b38952df50936f21a14a390dec355d04
DennisDaddy <softteez95@gmail.com>
2017-03-08 13:43:37 +0300

    generate scaffold

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   
 
+  resources :comments
   get 'categories/index'
 
   get 'categories/edit'
