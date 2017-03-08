b09c33bc23e3697c322e7f5f8d160a1b2dc184fd
DennisDaddy <softteez95@gmail.com>
2017-03-08 13:53:31 +0300

    remove scaffold css

--- db/migrate/20170308104648_create_post_comments.rb
+++ db/migrate/20170308104648_create_post_comments.rb
@@ -8,5 +8,6 @@ class CreatePostComments < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+    add_index :post_comments, :post_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304123413) do
+ActiveRecord::Schema.define(version: 20170308104648) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
