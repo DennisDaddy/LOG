33eb6a889c57b806f9f4eb77bdf5b3b9049ad52b
DennisDaddy <softteez95@gmail.com>
2017-03-09 15:53:24 +0300

    create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,11 +7,29 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+    
+  end
+
+  def create
+     @category = Category.new(category_params)
+     if @category.save
+      redirect_to categories_path, :notice => "Your category has been saved"
+    else
+      render 'new'
+     end
+    
   end
 
   def show
   	@category = Category.find(params[:id])
   	@title = @category.name
   	@posts = @category.posts
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
